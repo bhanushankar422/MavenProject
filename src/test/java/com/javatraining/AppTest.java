677159787b53606772f480e4b6787861b7d56ea5
Bhanu P <bhannushankar422@gmail.com>
2020-06-21 19:31:41 -0500

    Added sample maven project

---
+++ src/test/java/com/javatraining/AppTest.java
@@ -0,0 +1,19 @@
+package com.javatraining;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        System.out.println(" ****** In AppTest ********");
+        assertEquals(1, 1);
+    }
+}
